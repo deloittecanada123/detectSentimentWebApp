03c91f16de47394bed362bb68c267d83ffb42232
Aris <deloittecanada123@gmail.com>
2020-05-15 20:12:58 -0600

    Add project files.

---
+++ detectSentimentWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace detectSentimentWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
